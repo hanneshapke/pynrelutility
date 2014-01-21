766d7b282e31b68233457b87289b7073a7c02945
Hannes Hapke <hannes.hapke@gmail.com>
2014-01-21 17:53:24 -0300

    update of setup.py to correct information for pypi upload

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@
 # (at your option) any later version.
 
 """
-Distutils setup script for pyzillow.
+Distutils setup script for pynrelutility.
 
 """
 
@@ -23,28 +23,30 @@ except ImportError:
 from __version__ import VERSION
 
 setup(
-    name='pyzillow',
+    name='pynrelutility',
     version=VERSION,
     author='Hannes Hapke',
     author_email='hannes@renooble.com',
-    url='https://github.com/hanneshapke/pyzillow',
-    download_url='https://github.com/hanneshapke/pyzillow/archive/master.zip',
-    description='Python interface for Zillow\'s API. Currently supporting GetDeepSearchResults and GetUpdatedPropertyDetails API.',
-    long_description=open(os.path.join(os.path.dirname(__file__), 'README.txt'), 'r').read(),
-    py_modules=['pyzillow', 'pyzillowerrors', '__version__'],
-    provides=['pyzillow'],
+    url='https://github.com/hanneshapke/pynrelutility',
+    download_url='https://github.com/hanneshapke/pynrelutility/archive/master.zip',
+    description='Python interface for NREL\'s Utility API API. The API is providing \
+                utility names and rates for a given location',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r').read(),
+    py_modules=['nrel_utility', 'nrel_utility_errors', '__version__'],
+    provides=['nrel_utility'],
     requires=['requests'],
     install_requires=['requests >= 2.2.0'],
     classifiers=[
         'Development Status :: 3 - Alpha',
-        'Intended Audience :: Financial and Insurance Industry',
+        'Intended Audience :: Science/Research',
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2',
         'License :: OSI Approved :: BSD License',
         'Topic :: Internet',
         'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Scientific/Engineering',
     ],
-    keywords='zillow real estate rental xml api address zipcode',
+    keywords='nrel utility energy electricity rates api json US',
     license='MIT',
 )
